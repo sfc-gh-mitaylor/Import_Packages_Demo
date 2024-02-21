74beb41317289d1be2e6c58c5828668aeb75a650
Mike Taylor <michael.taylor@snowflake.com>
2024-02-21 15:20:20 +0000

    Example 1st Upload

---
+++ wheel_loader.py
@@ -0,0 +1,29 @@
+import sys
+import threading
+import fcntl
+import zipfile
+from pathlib import Path
+
+
+class FileLock:
+    def __enter__(self):
+        self._lock = threading.Lock()
+        self._lock.acquire()
+        self._fd = open('/tmp/wheel_loader.LOCK', 'w+')
+        fcntl.lockf(self._fd, fcntl.LOCK_EX)
+
+    def __exit__(self, type, value, traceback):
+        self._fd.close()
+        self._lock.release()
+
+
+def load(whl_name):
+    whl_path = Path(sys._xoptions['snowflake_import_directory']) / whl_name
+    extraction_path = Path('/tmp') / whl_name
+
+    with FileLock():
+        if not extraction_path.is_dir():
+            with zipfile.ZipFile(whl_path, 'r') as h_zip:
+                h_zip.extractall(extraction_path)
+
+    sys.path.append(str(extraction_path))
